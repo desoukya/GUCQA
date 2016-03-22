3cb3171ae724847f271659761764c2ae78da6d58
aasare <aasarekode@gmail.com>
2016-03-22 03:07:18 +0200

    Add questions show component

--- client/lib/router.js
+++ client/lib/router.js
@@ -5,6 +5,10 @@ angular.module('gucqa').config(function ($urlRouterProvider, $stateProvider, $lo
     .state('questions', {
       url: '/questions',
       template: '<questions-index></questions-index>'
+    })
+    .state('questionShow', {
+      url: '/questions/:questionId',
+      template: '<question-show></question-show>'
     });
 
   $urlRouterProvider.otherwise("/questions");
