a63a17dffae70b2cdfdd22c486d648c4cef1b8c9
Ahmed Saleh <ahmedzaleh7@gmail.com>
2016-03-23 00:59:43 +0200

    Sort questions by date descendingly

--- client/questions/index/index.component.js
+++ client/questions/index/index.component.js
@@ -10,11 +10,12 @@ angular.module('gucqa').directive('questionsIndex', function() {
 
       this.helpers({
         questions: () => {
-          return Questions.find({});
+          return Questions.find({}, { sort: { createdAt: -1 } });
         }
       });
 
       this.addQuestion = () => {
+        this.newQuestion.createdAt = new Date();
         Questions.insert(this.newQuestion);
         this.newQuestion = {};
       };
