1765e889c3f235e503d8f9b2e383c3d5cbf8eb93
Ahmed Saleh <ahmedzaleh7@gmail.com>
2016-03-22 03:14:39 +0200

    Add form to edit question in questions show component

--- client/questions/show/show.component.js
+++ client/questions/show/show.component.js
@@ -3,9 +3,29 @@ angular.module('gucqa').directive('questionShow', function () {
     restrict: 'E',
     templateUrl: 'client/questions/show/show.html',
     controllerAs: 'questionShow',
-    controller: function ($scope, $stateParams) {
-      var questionId = $stateParams.questionId;
-      this.question = Questions.findOne(questionId);
+    controller: function ($scope, $state, $stateParams, $reactive) {
+      $reactive(this).attach($scope);
+
+      this.helpers({
+        question: () => {
+          return Questions.findOne({ _id: $stateParams.questionId });
+        }
+      });
+
+      this.save = () => {
+        Questions.update({_id: $stateParams.questionId}, {
+          $set: {
+            content: this.question.content
+          }
+        }, (error) => {
+          if (error) {
+            console.log('An error occurred!');
+          } else {
+            console.log('Done!');
+            $state.go('questions');
+          }
+        });
+      };
     }
   }
 });
