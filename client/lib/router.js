9b1411944f89ad22dcbc904ec709e31caf451041
Ahmed Saleh <ahmedzaleh7@gmail.com>
2016-03-25 18:59:31 +0200

    Add Courses index page

--- client/lib/router.js
+++ client/lib/router.js
@@ -9,6 +9,10 @@ angular.module('gucqa').config(function ($urlRouterProvider, $stateProvider, $lo
     .state('questionShow', {
       url: '/questions/:questionId',
       template: '<question-show></question-show>'
+    })
+    .state('courses', {
+      url: '/courses',
+      template: '<courses-index></courses-index>'
     });
 
   $urlRouterProvider.otherwise("/questions");
