f6c2d8894d87f8ed413afff7758a15638506e789
raghda94 <raghdasoltan1994@gmaill.com>
2016-05-03 10:25:41 +0200

    added votes to answers and modified owner view in questions' panel heading

--- client/questions/index/index.component.js
+++ client/questions/index/index.component.js
@@ -30,6 +30,7 @@ angular.module('gucqa').directive('questionsIndex', function() {
 
         this.newQuestion = {};
       };
+      
 
       this.course = (courseId) => {
         return Courses.findOne({_id: courseId}).code;
